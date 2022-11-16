f1b919ac19d0d3874461be9fbe3ddaf2dd3cf604
MathLuke <110430212+MathLuke@users.noreply.github.com>
2022-11-15 20:00:47 -0500

    Completed MathFct4.cpp and Ex5.cpp

---
+++ cmake-build-debug/CMakeFiles/TEJ3MP.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TEJ3MP.dir/Exercise_5/Ex5.cpp.obj"
+  "CMakeFiles/TEJ3MP.dir/Exercise_5/Ex5.cpp.obj.d"
+  "TEJ3MP.exe"
+  "TEJ3MP.exe.manifest"
+  "TEJ3MP.pdb"
+  "libTEJ3MP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TEJ3MP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
