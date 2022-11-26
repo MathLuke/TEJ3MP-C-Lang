183669176658a5a61c5b940223d17faddd8fbe65
MathLuke <110430212+MathLuke@users.noreply.github.com>
2022-11-26 11:51:56 -0500

    Completed Ex 7 (Menu.cpp)

---
+++ cmake-build-debug/CMakeFiles/TEJ3MP.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/mathd/CLionProjects/TEJ3MP-C-Lang/Exercise 7/Menu.cpp" "CMakeFiles/TEJ3MP.dir/Exercise_7/Menu.cpp.obj" "gcc" "CMakeFiles/TEJ3MP.dir/Exercise_7/Menu.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TEJ3MP.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TEJ3MP.dir/Exercise_7/Menu.cpp.obj"
+  "CMakeFiles/TEJ3MP.dir/Exercise_7/Menu.cpp.obj.d"
+  "TEJ3MP.exe"
+  "TEJ3MP.exe.manifest"
+  "TEJ3MP.pdb"
+  "libTEJ3MP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TEJ3MP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
